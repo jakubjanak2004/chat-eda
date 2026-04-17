28c150e8b2af009dc2111cd3239104694d22cea5
Jakub Janak <janakja5@fel.cvut.cz>
2026-04-17 16:15:03 +0200

    updated to push images for amd64 as well

--- push-images-ghcr.sh
+++ push-images-ghcr.sh
@@ -1,8 +1,3 @@
-docker build -t ghcr.io/jakubjanak2004/chat-eda-user-service:latest -f user-service/Dockerfile .
-docker push ghcr.io/jakubjanak2004/chat-eda-user-service:latest
-
-docker build -t ghcr.io/jakubjanak2004/chat-eda-search-service:latest -f search-service/Dockerfile .
-docker push ghcr.io/jakubjanak2004/chat-eda-search-service:latest
-
-docker build -t ghcr.io/jakubjanak2004/chat-eda-ws-service:latest -f ws-service/Dockerfile .
-docker push ghcr.io/jakubjanak2004/chat-eda-ws-service:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/jakubjanak2004/chat-eda-user-service:latest -f user-service/Dockerfile . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/jakubjanak2004/chat-eda-search-service:latest -f search-service/Dockerfile . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/jakubjanak2004/chat-eda-ws-service:latest -f ws-service/Dockerfile . --push
