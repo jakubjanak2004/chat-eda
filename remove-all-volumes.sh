1af4125b595d75c4114b8670e42cbf4f38205e1a
Jakub Janak <janakja5@fel.cvut.cz>
2026-04-20 15:22:13 +0200

    todo comment added to remove-all-volumes.sh

--- remove-all-volumes.sh
+++ remove-all-volumes.sh
@@ -1,2 +1,3 @@
 # todo does not remove all volumes all on machines, only on local one
+# todo here we may ensure that the services all on removed so that all volumes can be removed, or create new script for that
 docker volume ls -q | xargs -r docker volume rm
