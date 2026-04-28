e135df7188bf133e85fdf641b3423d964fe248a6
Jakub Janak <janakja5@fel.cvut.cz>
2026-04-28 13:59:47 +0200

    added script to clean node

--- clean-node.sh
+++ clean-node.sh
@@ -1,3 +1,4 @@
 # todo does not remove all volumes all on machines, only on local one
 # todo here we may ensure that the services all on removed so that all volumes can be removed, or create new script for that
+docker system prune -af
 docker volume ls -q | xargs -r docker volume rm
